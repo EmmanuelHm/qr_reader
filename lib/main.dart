8fa9f8a29b0390c0226b7827267ef36931ed37de
Emmanuel Hernández Maqueda <emhermaq@gmail.com>
2021-09-08 21:14:13 -0500

    Interfaz y funcionalidad Qr activada

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/pages.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+
+// ignore: prefer_const_constructors
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UiProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR Reader',
+        initialRoute: 'home',
+        routes: {
+          // ignore: prefer_const_constructors
+          'home': (_) => HomePage(),
+          // ignore: prefer_const_constructors
+          'mapa': (_) => MapaPage(),
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: Colors.deepPurple,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/pages.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Historial'),
+        centerTitle: true,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: _HomePageBody(),
+
+      // Menu inferior y botón flotante
+      bottomNavigationBar: const CustopmNavigationBar(),
+      floatingActionButton: const ScanButtom(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  const _HomePageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Consumir Provider UiProvider
+    final uiProvider = Provider.of<UiProvider>(context);
+    // Cambiar para mostrar pagina
+    final currentIndex = uiProvider.selectedMenuOpt;
+    // Cambiar la vista dependiendo el provider
+    switch (currentIndex) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
+  }
+}
